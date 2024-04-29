9d4d54ef84a6373818e6cd1e7ce9f2510fce6d85
markus-moser <markus-moser@users.noreply.github.com>
2024-04-29 08:41:43 +0000

    Apply php-cs-fixer changes

--- src/PimcoreCustomerManagementFrameworkBundle.php
+++ src/PimcoreCustomerManagementFrameworkBundle.php
@@ -38,7 +38,8 @@ class PimcoreCustomerManagementFrameworkBundle extends AbstractPimcoreBundle imp
     /**
      * @just a test for CS fixer
      */
-    protected function getComposerPackageName(): string  {
+    protected function getComposerPackageName(): string
+    {
         return 'pimcore/customer-management-framework-bundle';
     }
 
